f0c04952bdf43e46b4ca0861eefb281193710008
K7S3 <ks2359@cornell.edu>
2025-07-05 16:35:06 +0000

    Added data

---
+++ data.js
@@ -0,0 +1,266 @@
+// Auto-generated data - Last updated: 2025-07-05T12:24:30.112317
+const websiteData = {
+  "lastUpdated": "2025-07-05T12:24:30.112317",
+  "projects": [
+    {
+      "name": "AutoEHR",
+      "description": "Automatic Health Record Generation",
+      "url": "https://github.com/K7S3/AutoEHR",
+      "homepage": null,
+      "language": "HTML",
+      "stars": 0,
+      "forks": 2,
+      "updated_at": "2025-05-31T18:48:26Z"
+    },
+    {
+      "name": "keshavanseshadri.github.io",
+      "description": "A website for Keshavan",
+      "url": "https://github.com/K7S3/keshavanseshadri.github.io",
+      "homepage": null,
+      "language": null,
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2025-05-11T13:18:06Z"
+    },
+    {
+      "name": "Attendance-System-Using-Image-Recognition",
+      "description": "Attendance System Using Image Recognition using Facenet",
+      "url": "https://github.com/K7S3/Attendance-System-Using-Image-Recognition",
+      "homepage": "",
+      "language": "Python",
+      "stars": 3,
+      "forks": 1,
+      "updated_at": "2025-04-12T11:22:47Z"
+    },
+    {
+      "name": "Multi-Modal-Models-for-Molecualr-Dynamics-Simulations.",
+      "description": "Framework to use Multi-Modal Models for Molecular Dynamics",
+      "url": "https://github.com/K7S3/Multi-Modal-Models-for-Molecualr-Dynamics-Simulations.",
+      "homepage": null,
+      "language": null,
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2025-03-13T18:45:47Z"
+    },
+    {
+      "name": "Synergii",
+      "description": "No description available",
+      "url": "https://github.com/K7S3/Synergii",
+      "homepage": null,
+      "language": "JavaScript",
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2025-03-05T13:51:39Z"
+    },
+    {
+      "name": "Endless-Runner",
+      "description": "WebGL custom implementation(built from scratch) of an endless runner game like subway surfers",
+      "url": "https://github.com/K7S3/Endless-Runner",
+      "homepage": "",
+      "language": "JavaScript",
+      "stars": 2,
+      "forks": 0,
+      "updated_at": "2024-08-13T20:32:37Z"
+    },
+    {
+      "name": "Arga",
+      "description": "Linear Regression Model",
+      "url": "https://github.com/K7S3/Arga",
+      "homepage": null,
+      "language": "Jupyter Notebook",
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2024-07-03T02:59:43Z"
+    },
+    {
+      "name": "Prudential-MLE",
+      "description": "Prudential MLE assesment",
+      "url": "https://github.com/K7S3/Prudential-MLE",
+      "homepage": null,
+      "language": "Jupyter Notebook",
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2024-07-01T15:45:47Z"
+    },
+    {
+      "name": "fitloop-server",
+      "description": "Backend for fitloop",
+      "url": "https://github.com/K7S3/fitloop-server",
+      "homepage": null,
+      "language": "Python",
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2024-04-04T04:29:37Z"
+    },
+    {
+      "name": "Food-Image-Recognition",
+      "description": "A system that takes food images as an input, recognizes the food automatically and gives the nutritional-facts as an output.",
+      "url": "https://github.com/K7S3/Food-Image-Recognition",
+      "homepage": "",
+      "language": null,
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2024-04-04T03:29:42Z"
+    },
+    {
+      "name": "BashScript-files",
+      "description": "Contain some bash scripts to perform certain tasks",
+      "url": "https://github.com/K7S3/BashScript-files",
+      "homepage": null,
+      "language": "Shell",
+      "stars": 1,
+      "forks": 0,
+      "updated_at": "2024-02-13T06:11:16Z"
+    },
+    {
+      "name": "Spotify-Transformer",
+      "description": "An app that allows users to download Spotify tracks, providing lyrics, English phonetics for karaoke, and word meanings to enhance the listening experience using OpenAI's GPT-4",
+      "url": "https://github.com/K7S3/Spotify-Transformer",
+      "homepage": "",
+      "language": "Jupyter Notebook",
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2024-01-30T00:53:24Z"
+    },
+    {
+      "name": "K7-Shell",
+      "description": "Custom shell created using C from scratch.",
+      "url": "https://github.com/K7S3/K7-Shell",
+      "homepage": "",
+      "language": "C",
+      "stars": 1,
+      "forks": 0,
+      "updated_at": "2024-01-30T00:06:42Z"
+    },
+    {
+      "name": "cs5356-docs",
+      "description": "No description available",
+      "url": "https://github.com/K7S3/cs5356-docs",
+      "homepage": null,
+      "language": "JavaScript",
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2024-01-28T21:18:31Z"
+    },
+    {
+      "name": "cal.com",
+      "description": "Scheduling infrastructure for absolutely everyone.",
+      "url": "https://github.com/K7S3/cal.com",
+      "homepage": "https://cal.com",
+      "language": null,
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2023-12-02T16:14:29Z"
+    },
+    {
+      "name": "Franchise",
+      "description": "Product Studio - Franchise Sponsorship Company",
+      "url": "https://github.com/K7S3/Franchise",
+      "homepage": null,
+      "language": "SCSS",
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2023-11-26T18:38:31Z"
+    },
+    {
+      "name": "Module-3",
+      "description": "Module 3 - Efficiency",
+      "url": "https://github.com/K7S3/Module-3",
+      "homepage": null,
+      "language": null,
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2023-11-18T19:54:25Z"
+    },
+    {
+      "name": "GPCR_ML_Residue_Importance",
+      "description": "This repo contains the code used in our analysis of prediciting the importance of residues in the binding pocket of GPCR in determining the state transition observed in adrenaline.",
+      "url": "https://github.com/K7S3/GPCR_ML_Residue_Importance",
+      "homepage": null,
+      "language": "Jupyter Notebook",
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2023-05-25T14:34:33Z"
+    },
+    {
+      "name": "ASCII-Mario",
+      "description": "The supermario game using ascii characters on terminal",
+      "url": "https://github.com/K7S3/ASCII-Mario",
+      "homepage": "",
+      "language": "Python",
+      "stars": 4,
+      "forks": 1,
+      "updated_at": "2023-02-13T23:04:43Z"
+    },
+    {
+      "name": "ML4NS-Project",
+      "description": "This repo contains the code for the ML4NS project.",
+      "url": "https://github.com/K7S3/ML4NS-Project",
+      "homepage": null,
+      "language": "Jupyter Notebook",
+      "stars": 0,
+      "forks": 0,
+      "updated_at": "2021-05-01T04:19:21Z"
+    }
+  ],
+  "publications": [
+    {
+      "title": "A Computational Study on GPCR Activation Mechanisms: Insights from Adrenaline Binding and G-Protein Dissociation",
+      "authors": "Keshavan Seshadri et al.",
+      "venue": "International Institute of Information Technology, Hyderabad",
+      "year": "2023",
+      "abstract": "GPCRs are the most prominent family of membrane proteins that serve as major targets for one-third of the drugs produced. A detailed understanding of the molecular mechanism of drug-induced activation and inhibition of GPCRs is crucial for the rational design of novel therapeutics.",
+      "citations": 0,
+      "url": "https://scholar.google.com/citations?user=3M3fxRYAAAAJ"
+    }
+  ],
+  "timeline": [
+    {
+      "date": "Present",
+      "title": "Prudential Financial",
+      "description": "Working on innovative financial technology solutions and AI applications",
+      "type": "work"
+    },
+    {
+      "date": "2024",
+      "title": "Break Through Tech Mentor",
+      "description": "Mentoring students to help them get into Tech and AI",
+      "type": "volunteer"
+    },
+    {
+      "date": "2023",
+      "title": "Cornell University",
+      "description": "Graduated with degree in Computer Science",
+      "type": "education"
+    },
+    {
+      "date": "2023",
+      "title": "IIIT Hyderabad Research",
+      "description": "Published research on GPCR Activation Mechanisms",
+      "type": "research"
+    },
+    {
+      "date": "2022",
+      "title": "LinkedIn Learning",
+      "description": "Completed SQL: Data Reporting and Analysis certification",
+      "type": "certification"
+    }
+  ],
+  "news": [
+    {
+      "date": "January 2025",
+      "title": "Won Grand Prize at AI Berkeley Hackathon",
+      "description": "Our team built ChipChat, an innovative solution that won the Grand Prize at the 2025 AI Berkeley Hackathon!"
+    },
+    {
+      "date": "December 2024",
+      "title": "Panel Discussion on Explainable AI",
+      "description": "Participated in a panel discussion on Explainability and Transparency in Autonomous Agents."
+    },
+    {
+      "date": "October 2024",
+      "title": "Mentoring at Break Through Tech",
+      "description": "Started mentoring students to help them get into Tech and AI through Break Through Tech program."
+    }
+  ]
+};
